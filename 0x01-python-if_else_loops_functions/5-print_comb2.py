0aebce341098e111f59e701223171db92c57bbd0
Austin <Martinaustin319@gmail.com>
2023-09-06 21:02:48 +0000

    if\else

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
-	if i != 99:
-		if i < 10:
-			print("0" + str(i), end=", ")
-		else:
-			print(i, end=", ")
-	else:
-		print(i)
+	print("{:02}".format(i), ", ", sep='', end='')
+print("99")
