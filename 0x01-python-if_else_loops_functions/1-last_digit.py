64a55843ee9aaae93fa178ffc7473bd03893b1b4
Austin <Martinaustin319@gmail.com>
2023-09-06 21:30:04 +0000

    if\else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	digit = (abs(number) % 10) * -1
+    digit = (abs(number) % 10) * -1
 else:
-	digit = number % 10 
+    digit = number % 10 
 if digit == 0:
-	print("Last digit of", number, "is", digit, "and is 0")
+    print("Last digit of", number, "is", digit, "and is 0")
 if (digit < 6) and (digit != 0):
-	print("Last digit of", number, "is", digit, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", digit, "and is less than 6 and not 0")
 if digit > 5:
-	print("Last digit of", number, "is", digit, "and is greater than 5")
+    print("Last digit of", number, "is", digit, "and is greater than 5")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-	result = ""
-	for i in str:
-		character = i
-		if ord(i) >= ord('a') and ord(i) <= ord('z'):
-			character = chr(ord(i) - 32)
-		result += character
-	print("{}".format(result))
+    result = ""
+    for i in str:
+        character = i
+	if ord(i) >= ord('a') and ord(i) <= ord('z'):
+		character = chr(ord(i) - 32)
+	result += character
+    print("{}".format(result))
