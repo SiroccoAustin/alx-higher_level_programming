e083e901ee2b2313490695aca001138c25bb08ea
Austin <Martinaustin319@gmail.com>
2023-09-11 11:15:24 +0000

    python data structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    First_letter = sentence[0] if sentence != None else None
+    First_letter = sentence[0] if sentence else None
     new_tuple = (length, First_letter)
     return new_tuple
