2af361377011ad583fa20ac8354aaef178618749
Austin <Martinaustin319@gmail.com>
2023-09-11 11:17:16 +0000

    python data structures

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,7 +8,3 @@ def max_integer(my_list=[]):
             num = my_list[i]
         i += 1
     return num
-
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
