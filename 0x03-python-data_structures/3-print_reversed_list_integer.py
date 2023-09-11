0b5edfa216d2c09f9f3a9c8166b3bab1c128706c
Austin <Martinaustin319@gmail.com>
2023-09-11 10:11:49 +0000

    python data structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def print_reversed_list_integer(my_list=[]):
     num = len(my_list)
     while num > 0:
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,5 +6,3 @@ def no_c(my_string):
         if i != "C" and i != "c":
             new_str += i
     return new_str
-
-print(no_c("Chicago"))
