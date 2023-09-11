2f61c4985d20934a4762a9e3059e90df6abc4d91
Austin <Martinaustin319@gmail.com>
2023-09-11 10:32:39 +0000

    python data structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     while i < len(matrix):
         j = 0
         for j in matrix[i]:
-            print("{:d}".format(j, end=" "))
+            print("{:d}".format(j), end=" ")
             j+= 1
         print("\n")
         i += 1
