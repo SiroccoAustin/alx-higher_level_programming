d86b3f561d3c58cf42568c87a683adc9242fda38
Austen Dlamini <Martinaustin319@gmail.com>
2023-09-13 09:46:05 +0200

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 
-print("hello world!");
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    i = 0
+    while len(matrix) > i:
+        sub_array = []
+        j = 0
+        for j in matrix[i]:
+            power = j ** 2
+            sub_array.append(power)
+            j += 1
+        new_matrix.append(sub_array)
+        i += 1
+    return new_matrix
