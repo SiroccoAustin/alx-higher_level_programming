5642200e7265678d4a9d0199e08b5d47f152988c
Austen Dlamini <Martinaustin319@gmail.com>
2023-09-14 09:39:39 +0200

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,9 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    high_score = None
-    max_value = float("-inf")
+    high_score = 0
     for key, value in a_dictionary.items():
-        if value > max_value:
+        if value > high_score:
             high_score = value
-            max_value = value
     return high_score
