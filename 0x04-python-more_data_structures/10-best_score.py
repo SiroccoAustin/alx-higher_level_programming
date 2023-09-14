7783985c98cb2222e6c2b71e42432265e5c5adb5
Austen Dlamini <Martinaustin319@gmail.com>
2023-09-14 09:06:36 +0200

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,8 @@
 def best_score(a_dictionary):
     high_score = 0
     for key, value in a_dictionary.items():
+        if not value:
+            return None
         if value > high_score:
             high_score = value
     return high_score
