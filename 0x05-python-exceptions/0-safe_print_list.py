ddfc02917038c7308aa9a779e7a2994650af9175
Austen Dlamini <Martinaustin319@gmail.com>
2023-09-25 12:21:59 +0200

    safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        while (count < x):
+            print(my_list[count])
+            count += 1
+    except IndexError:
+        pass
+    return count
+
+    
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
