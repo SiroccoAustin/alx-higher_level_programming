0914e361b59ae25e2b6c92602f1901509d37abca
Austen Dlamini <Martinaustin319@gmail.com>
2023-09-26 19:40:10 +0200

    print range times

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""defining a class"""
+
+
+class Square:
+    """Creating a class that define size"""
+    def __init__(self, size=0):
+        """initialize"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    
+    def area(self):
+        """calculates the area"""
+        return self.__size * self.__size
+    
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """print # in range of size"""
+        if self.__size == 0:
+            print()
+        print("#" * self.__size)
