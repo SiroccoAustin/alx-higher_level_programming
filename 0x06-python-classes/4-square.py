f389e66b1545999ba2079e251f6ffb9baaea8e26
Austen Dlamini <Martinaustin319@gmail.com>
2023-09-26 19:24:54 +0200

    setter and getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,6 +5,7 @@
 class Square:
     """Creating a class that define size"""
     def __init__(self, size=0):
+        """initialize"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
