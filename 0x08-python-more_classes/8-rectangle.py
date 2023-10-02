f5a08ff7448027eeff380be4794e323b62ea1699
Austen Dlamini <Martinaustin319@gmail.com>
2023-10-02 14:36:48 +0200

    more python classes

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -75,6 +75,6 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_2.area > rect_1.area:
+        if rect_2.area() > rect_1.area():
             return rect_2
         return rect_1
