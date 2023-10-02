5506128d7badc7d9de996afb083e9dace837bdf5
Austen Dlamini <Martinaustin319@gmail.com>
2023-10-02 14:00:16 +0200

    more python classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -41,9 +41,9 @@ class Rectangle:
     def height(self, value):
         """setter"""
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
