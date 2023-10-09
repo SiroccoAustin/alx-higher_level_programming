5e475b96d046e59b9314a62e3a481443fb8d6986
Austen Dlamini <Martinaustin319@gmail.com>
2023-10-09 23:07:56 +0200

    inherit

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -16,15 +16,18 @@ class BaseGeometry:
             raise ValueError("{} must be greater than 0".format(name))
         
 class Rectangle(BaseGeometry):
+    """Define Rectangle class"""
     def __init__(self, width, height):
+        """initialize private variables"""
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
-        self.width = width
-        self.height = height
+        self.__height = height
     def area(self):
         """Define area of the Rectangle"""
         return self.width * self.height
     
     def __str__(self):
+        """returns a string to print"""
         return "[Rectangle] {:d}/{:d}".format(self.width, self.height)
 
