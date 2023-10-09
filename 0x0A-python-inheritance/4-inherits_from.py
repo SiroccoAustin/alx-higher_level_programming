a72fe7cc62dbd92b70676ae9f47ab5e76469a381
Austen Dlamini <Martinaustin319@gmail.com>
2023-10-09 22:51:35 +0200

    inherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,4 +3,6 @@
 """Function that checks if an instance inherits from class"""
 
 def inherits_from(obj, a_class):
+    if type(obj) == a_class:
+        return False
     return isinstance(obj, a_class)
