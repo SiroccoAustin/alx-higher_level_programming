2637bd7e17606a0a996d704cbce539f1e30cf51a
Austen Dlamini <Martinaustin319@gmail.com>
2023-10-09 23:27:53 +0200

    inherit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,6 +3,11 @@
 """Class that inherits from list"""
 
 class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
         """Function that prints a sorted list"""
         print(sorted(self))
