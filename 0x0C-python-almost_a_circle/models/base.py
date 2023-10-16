0f6a0dea99fdb3924afacfb9b64343cb3c600980
Austen Dlamini <Martinaustin319@gmail.com>
2023-10-16 20:49:36 +0200

    python almost circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,7 +7,7 @@ class Base:
     """Base geometry class"""
     __nb_objects = 0
     def __init__(self, id=None):
-        if id is not None:
+        if id is None:
             self.id = Base.__nb_objects
             Base.__nb_object += 1
         else:
