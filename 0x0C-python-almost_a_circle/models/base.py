5490bffedb4265fb25ee6fa49ee56b28f308ae17
Austen Dlamini <Martinaustin319@gmail.com>
2023-10-16 21:06:45 +0200

    python almost circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,22 +4,12 @@ import json
 """defining Base class"""
 
 class Base:
-    """Base model.
-
-    This Represents the "base" for all other classes in project 0x0C*.
-
-    Private Class Attributes:
-        __nb_object (int): Number of instantiated Bases.
-    """
+    """Base model"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base.
-
-        Args:
-            id (int): The identity of the new Base.
-        """
+        """Initialize a new Base"""
         if id is not None:
             self.id = id
         else:
