95e32f75ac88f0d99b771ae169755c204a2d14c0
Austen Dlamini <Martinaustin319@gmail.com>
2023-10-18 12:57:17 +0200

    python almost circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,16 @@ from base import Base
 class Rectangle(Base):
     """Class for drawing a Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a new Rectangle instance.
+
+        Args:
+            width: width of the Rectangle
+            height: height of the Rectangle
+            x: spaces
+            y: spaces
+            id (int, optional): The ID to assign to the instance. If not provided, a new ID will be generated.
+        """
         self.__width = width
         self.__height = height
         self.__x = x
