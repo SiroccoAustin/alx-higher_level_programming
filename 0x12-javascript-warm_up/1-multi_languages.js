caf5dee0812740e35e27008e2d93861ed57da434
Austin <Martinaustin319@gmail.com>
2023-12-11 12:51:27 -0800

    javascript warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-let myVar1 = "C is fun";
-let myVar2 = "Python is cool";
-let myVar3 = "JavaScript is amazing";
-console.log(myVar1);
-console.log(myVar2);
-console.log(myVar3);
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
