73df49ae1512db28d7cd7ccbd88471adc42a6c28
Austin <Martinaustin319@gmail.com>
2023-12-17 07:39:34 -0800

    ORM

---
+++ 0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = conn.cursor()
+    c = execute("SELECT * FROM states ORDER BY id ASC")
+    rows = c.fetchall()
+    for eachRow in rows:
+        print(eachRow)
+    c.close()
+    conn.close()
