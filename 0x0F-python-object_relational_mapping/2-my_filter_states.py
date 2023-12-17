97a9a18503e6ef4a395affcd59ebe9bc68b48ef8
Austin <Martinaustin319@gmail.com>
2023-12-17 09:52:16 -0800

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,6 +6,9 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     my_spot = sys.argv[4]
     c = conn.cursor()
