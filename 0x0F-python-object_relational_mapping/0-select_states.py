4430fb1aca0af85118bc7b1935fd3f8d469dc9b2
Austin <Martinaustin319@gmail.com>
2023-12-17 09:28:11 -0800

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""Lists all states in the database"""
+
 import MySQLdb
 import sys
 
@@ -7,7 +9,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 4:
         exit(1)
 
-    conn = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     c = conn.cursor()
     c.execute("SELECT * FROM states ORDER BY id ASC")
     rows = c.fetchall()
