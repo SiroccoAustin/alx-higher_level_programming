92990e7cff160f2d7c14fb2c9408f013810cc8a7
Austin <Martinaustin319@gmail.com>
2023-12-17 09:49:53 -0800

    ORM

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Filter the state by my state"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    my_spot = sys.argv[4]
+    c = conn.cursor()
+    c.execute("SELECT * FROM states WHERE name = '{}'".format(my_spot))
+    rows = c.fetchall()
+    for eachRow in rows:
+        print(eachRow)
+    c.close()
+    conn.close()
