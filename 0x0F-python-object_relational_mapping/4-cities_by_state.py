55fc462cfdeb26ddb577c0da033036967194cb72
Austin <Martinaustin319@gmail.com>
2023-12-22 12:28:35 -0800

    ORM

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if (sys.argv) < 4:
+    if len(sys.argv) < 4:
         sys.exit(1)
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     c = conn.cursor()
