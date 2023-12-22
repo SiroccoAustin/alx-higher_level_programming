6e1104d71ce62a2081d435fcf2de634da29e795e
Austin <Martinaustin319@gmail.com>
2023-12-22 13:35:26 -0800

    ORM

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,6 +4,19 @@ import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmakerfrom sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    """Define State"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+    def repr(self):
+        return f"{self.id}: {self.name}"
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
