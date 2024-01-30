f934b17b5ae5e0373658226ea78de0c1187b12bc
Austin <Martinaustin319@gmail.com>
2024-01-30 13:25:35 -0800

    web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
-
-if (process.argv.length < 3) {
-	  console.error(`Usage: ./${process.argv[1]} <URL>`);
-	  process.exit(1);
-}
-
 const url = process.argv[2];
-
-request.get(url, (err, res) => {
-	  if (err) {
-		      console.error('Error:', err.message);
-		    }
-	  const taskObj = {};
-	  const data = JSON.parse(res.body);
-	  data.map((obj) => {
-		      if (!(taskObj[`${obj.userId}`])) { taskObj[`${obj.userId}`] = 0; }
-		      if (obj.completed) { taskObj[`${obj.userId}`] = taskObj[`${obj.userId}`] + 1; }
-		      return taskObj;
-		    });
-	  console.log(taskObj);
+let count = 0
+request.get(url, (error, response, body) => {
+	if (error){
+		console.error(error);
+	}
+	todos = JSON.parse(body);
+	const completed = {};
+	for (const i in todos){
+		const task = todos[i];
+		if (task.completed === true){
+			if (completed[task.userId] === undefined){
+				count = 1
+				completed[task.userId] = count;
+			}else{
+				count++;
+				completed[task.userId] = count;
+			}
+		}
+	}
+	console.log(completed);
 });
